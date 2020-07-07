636fd8d9f4e05948c1d3538ba7c7d7c0a87c36e2
veronika333 <vera3@inbox.ru>
2020-07-07 21:50:10 +0300

    added button, which removes individual items

--- src/componnents/GroceryItem.js
+++ src/componnents/GroceryItem.js
@@ -4,6 +4,7 @@ const GroceryItem = (props) => {
         <div className="name">
             <h3>Product: {props.name}</h3>
     <h3>Calories: {props.calories}</h3>
+    <button name={props.name} onClick={props.onClick}>Remove this product</button>
         </div>
      );
 }
--- src/componnents/GroceryList.js
+++ src/componnents/GroceryList.js
@@ -18,13 +18,20 @@ const filteredProducts = product.filter(value=> value.calories < 250);
 setProduct(filteredProducts);
 }
 
+const removeOne = (e) => {
+console.log(e.target.name);
+const removeOneProduct = product.filter(value => value.name !== e.target.name);
+setProduct(removeOneProduct);
+}
+
     return ( 
         <div>
         <h1>Grocery List</h1>
-        <h2>Click the button and remove all the products, which are over 250 calories</h2>
+        <h2>Click the button in the bottom and remove all the products, which are over 250 calories</h2>
+        <h2>Or click the button and remove individual product</h2>
         {
             product.map((value, index) => {
-               return <GroceryItem key={`${index} ${value.name} ${value.calories}`} name={value.name} calories={value.calories} />
+               return <GroceryItem key={`${index} ${value.name} ${value.calories}`} name={value.name} calories={value.calories} onClick={removeOne}  />
             })
         }
         <button onClick={removeUnhealthy}>Remove Unhealthy Over 250 Calories</button>
