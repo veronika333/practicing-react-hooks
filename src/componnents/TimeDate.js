2ba6e4e583237066f9e27025f577c26c5541ae28
veronika333 <vera3@inbox.ru>
2020-07-13 11:19:30 +0300

    time and mouse position added, they are updated

--- src/componnents/TimeDate.js
+++ src/componnents/TimeDate.js
@@ -1,17 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
+const positionXY = {
+x: 0,
+y: 0
+}
+
 const TimeDate = () => {
     const [time, setTime] = useState(Date);
+const [xy, setXY] = useState(positionXY);
+
 useEffect(() => {
    let handle = setInterval(() => {
        setTime(Date)}, 1000);
    return () => {clearInterval(handle)};
 })
 
+const mouseMoveHandler = (e) => {
+ setXY({
+     x:e.clientX,
+     y:e.clientY
+ })
+}
+
+useEffect(() => {
+window.addEventListener('mousemove', mouseMoveHandler)
+}, []);
+
     return ( 
         <div>
-            <h1>Time and date is updated every second</h1>
+            <h1>Time and date is updated every second and you can see mouse position in the screen</h1>
     <h2>The current date and time: {time}</h2>
+    <p>You mouse position in the screen is:</p>
+    <p>x: {xy.x}, y: {xy.y}</p>
         </div>
      );
 }
